b74fbd8ca492fcff6df5912ce718e9be7ecab637
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-03-30 21:15:31 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,23 @@
-from dataclasses import dataclass
+import csv
+import time
+
+from selenium import webdriver
+from dataclasses import dataclass, fields, astuple
 from urllib.parse import urljoin
 
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+PAGES = {
+    "home": HOME_URL,
+    "computers": f"{HOME_URL}computers",
+    "laptops": f"{HOME_URL}computers/laptops",
+    "tablets": f"{HOME_URL}computers/tablets",
+    "phones": f"{HOME_URL}phones",
+    "touch": f"{HOME_URL}phones/touch",
+}
 
 
 @dataclass
@@ -15,8 +29,64 @@ class Product:
     num_of_reviews: int
 
 
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome(options=op)
+
+FIELDS = [field.name for field in fields(Product)]
+
+
+def extract_product(product: driver) -> Product:
+    return Product(
+        title= product.find_element(By.CSS_SELECTOR,
+                                    "a.title").get_attribute("title"),
+        description= product.find_element(By.CSS_SELECTOR,
+                                          "p.description").text,
+        price=float(
+            product.find_element(By.CSS_SELECTOR, "h4.price")
+            .text[1:]),
+        rating=len(
+            product.find_elements(By.CSS_SELECTOR, "span.ws-icon-star")
+        ),
+        num_of_reviews=int(
+            product.find_element(By.CSS_SELECTOR, "p.review-count")
+            .text.split()[0]
+        ),
+    )
+
+
+def parse_page(url: str) -> list[Product]:
+    driver.get(url)
+    try:
+        while True:
+            button = (
+                driver.find_element(
+                    By.CSS_SELECTOR, ".ecomerce-items-scroll-more"
+                )
+            )
+            if "display: none" in button.get_attribute("style"):
+                raise NoSuchElementException
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(0.1)
+    except NoSuchElementException:
+        products = []
+        for product in driver.find_elements(By.CSS_SELECTOR, "div.card-body"):
+            products.append(extract_product(product))
+        return products
+
+
+def write_to_csv(products: list[Product], file_name: str) -> None:
+    with open(f"{file_name}.csv", "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(FIELDS)
+        writer.writerows(astuple(product) for product in products)
+
+
 def get_all_products() -> None:
-    pass
+    for file_name, url in PAGES.items():
+        products = parse_page(url)
+        write_to_csv(products, file_name)
+    driver.close()
 
 
 if __name__ == "__main__":
